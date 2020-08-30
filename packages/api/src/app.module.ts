b394c2452f68c86de2c74228ed793303eaedbc05
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-08-30 12:04:48 -0400

    add typeorm and db

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { TalentModule } from './talent/talent.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       uploads: true,
