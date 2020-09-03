04a440cb46ec0de6770b6706158f47cda2f07a6a
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-09-03 14:40:49 -0400

    remove unused variables and imports

--- packages/api/src/employer/employer.guard.ts
+++ packages/api/src/employer/employer.guard.ts
@@ -1,12 +1,8 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-// import { Observable } from 'rxjs';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { EmployerService } from './employer.service';
 
 @Injectable()
 export class EmployerGuard implements CanActivate {
-  constructor(private readonly employerService: EmployerService) {}
-
   canActivate(context: ExecutionContext): boolean {
     const ctx = GqlExecutionContext.create(context);
 
