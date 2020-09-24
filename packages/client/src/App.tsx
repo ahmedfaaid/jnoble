b2f956127fa88a593a40d7dd8d5ef0fe20206189
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-09-24 13:37:44 -0400

    add home, login, signup routes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div>
-      <h1>JNoble Client</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+      </Switch>
+    </Router>
   );
 }
 
