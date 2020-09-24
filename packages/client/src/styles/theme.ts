c9db970cf4ded56a03350c38e0d2e98553544b9f
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-09-24 14:05:57 -0400

    Add styled components theme

---
+++ packages/client/src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primaryColor: string;
+      secondaryColor: string;
+      tertiaryColor: string;
+      white: string;
+      black: string;
+    };
+  }
+}
+
+export const theme: DefaultTheme = {
+  colors: {
+    primaryColor: '#797a7e',
+    secondaryColor: '#d8d3cd',
+    tertiaryColor: '#e0ece4',
+    white: '#ffffff',
+    black: '#000000',
+  },
+};
