2c29debb20c29fe2f83a1bf6e5fa276820ed51e6
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2020-10-05 11:23:09 -0400

    add about, employer, howitworks and talent routes

---
+++ packages/client/src/pages/Employer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+function Employer() {
+  return (
+    <Layout>
+      <h1>Employer Page</h1>
+    </Layout>
+  );
+}
+
+export default Employer;
