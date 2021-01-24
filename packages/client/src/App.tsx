36adedd0ced5cef3cd72854e681633ad08cbb2af
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-01-24 13:55:10 -0500

    add candidates route

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Candidates from './pages/Candidates';
 import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path='/candidates'>
+          <Candidates />
+        </Route>
         <Route path='/'>
           <Home />
         </Route>
