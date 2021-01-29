ad52bd554dc2ace2c503849268e4f1809500aacc
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-01-28 22:28:39 -0500

    fix loading spinner

--- packages/client/src/styles/utils.ts
+++ packages/client/src/styles/utils.ts
@@ -1,23 +1,28 @@
 import styled from 'styled-components';
 import { theme as t } from './Theme';
 
+export const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 65rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Loading = styled.div`
-  display: inline-block;
-  width: 80px;
-  height: 80px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  display: block;
+  width: 20rem;
+  height: 20rem;
+  margin: auto;
   &:after {
     content: ' ';
     display: block;
-    width: 64px;
-    height: 64px;
+    width: 18rem;
+    height: 18rem;
     margin: 8px;
     border-radius: 50%;
-    border: 6px solid ${t.colors.blue[1]};
-    border-color: ${t.colors.blue[1]} transparent ${t.colors.blue[1]}
+    border: 6px solid ${t.colors.primary[1]};
+    border-color: ${t.colors.primary[1]} transparent ${t.colors.primary[1]}
       transparent;
     animation: lds-dual-ring 1.2s linear infinite;
   }
