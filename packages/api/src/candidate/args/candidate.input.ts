a1138f1d89fbebd33ea3c5099da9dc9811480ac1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-01-31 00:56:01 -0500

    candidate update input

--- packages/api/src/candidate/args/candidate.input.ts
+++ packages/api/src/candidate/args/candidate.input.ts
@@ -1,4 +1,5 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { AddressInput } from 'src/address/address.entity';
 
 // TODO: make some of these fields nullable
 @InputType()
@@ -43,5 +44,53 @@ export class CandidateInput {
   statusInCanada: string;
 
   @Field()
+  available: boolean;
+}
+
+@InputType()
+export class CandidateUpdateInput {
+  @Field({ nullable: true })
+  firstName: string;
+
+  @Field({ nullable: true })
+  lastName: string;
+
+  @Field({ nullable: true })
+  middleName: string;
+
+  @Field({ nullable: true })
+  preferredName: string;
+
+  @Field({ nullable: true })
+  dateOfBirth: string;
+
+  @Field({ nullable: true })
+  jobTitle: string;
+
+  @Field({ nullable: true })
+  phone: string;
+
+  @Field({ nullable: true })
+  email: string;
+
+  @Field({ nullable: true })
+  address: AddressInput;
+
+  @Field(() => [String], { nullable: true })
+  languages: string[];
+
+  @Field(() => [String], { nullable: true })
+  skills: string[];
+
+  @Field({ nullable: true })
+  validDriversLicense: boolean;
+
+  @Field({ nullable: true })
+  ownVehicle: boolean;
+
+  @Field({ nullable: true })
+  statusInCanada: string;
+
+  @Field({ nullable: true })
   available: boolean;
 }
