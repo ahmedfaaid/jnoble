ceedae5bee48b6055a4b171e8d75eb209495080e
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-01-31 20:17:23 -0500

    add address and province type

--- packages/client/src/types/index.ts
+++ packages/client/src/types/index.ts
@@ -2,6 +2,18 @@ export interface Candidate {
   id: number;
   firstName: string;
   lastName: string;
+  jobTitle: string;
   phone: string;
   email: string;
+  address: Address;
+}
+
+interface Address {
+  city: string;
+  province: Province;
+  country: string;
+}
+
+interface Province {
+  name: string;
 }
