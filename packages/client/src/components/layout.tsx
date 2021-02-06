42d06d6c6cc92e0e5189ab6c25db1d811504b532
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-06 14:21:24 -0500

    add page tile

--- packages/client/src/components/layout.tsx
+++ packages/client/src/components/layout.tsx
@@ -27,6 +27,7 @@ export default function Layout({ children, page }: LayoutProps) {
           href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,400&display=swap'
           rel='stylesheet'
         />
+        <title>{page} - JNoble</title>
       </Helmet>
       <div>
         <PageLayout>
