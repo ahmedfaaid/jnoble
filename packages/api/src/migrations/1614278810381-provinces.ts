b9b5c1b749800e952157a23469063c776d3cb73a
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2021-02-25 17:01:47 -0500

    add provinces migration

---
+++ packages/api/src/migrations/1614278810381-provinces.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class provinces1614278810381 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        INSERT INTO province (name, abbr) values ('Alberta', 'AB');
+        INSERT INTO province (name, abbr) values ('British Columbia', 'BC');
+        INSERT INTO province (name, abbr) values ('Manitoba', 'MB');
+        INSERT INTO province (name, abbr) values ('New Brunswick', 'NB');
+        INSERT INTO province (name, abbr) values ('Newfoundland and Labrador', 'NL');
+        INSERT INTO province (name, abbr) values ('Northwest Territories', 'NT');
+        INSERT INTO province (name, abbr) values ('Nova Scotia', 'NS');
+        INSERT INTO province (name, abbr) values ('Nunavut', 'NU');
+        INSERT INTO province (name, abbr) values ('Ontario', 'ON');
+        INSERT INTO province (name, abbr) values ('Prince Edward Island', 'PE');
+        INSERT INTO province (name, abbr) values ('Quebec', 'QC');
+        INSERT INTO province (name, abbr) values ('Saskatchewan', 'SK');
+        INSERT INTO province (name, abbr) values ('Yukon Territory', 'YT');
+      `);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
